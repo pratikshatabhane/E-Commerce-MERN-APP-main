ee627895e99c5f20d5199cbf0ebbb42d6b8318b6
Pratiksha Tabhane <103399336+pratikshatabhane@users.noreply.github.com>
2024-07-01 13:59:41 +0530

    Delete Backend directory

--- Backend/routes/reservationRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import send_reservation from "../controller/reservation.js";
-
-const router = express.Router();
-
-router.post("/send", send_reservation);
-
-export default router;
--- Backend/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
